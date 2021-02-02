9ded7540b2dca42f36947b6693c79e8be633e28c
filip <fbielejec@gmail.com>
2021-02-02 20:40:57 +0100

    wip: consumer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use log::{debug, info, warn, error};
 use rdkafka::client::ClientContext;
 use rdkafka::config::{ClientConfig, RDKafkaLogLevel};
 use rdkafka::consumer::stream_consumer::StreamConsumer;
@@ -7,7 +8,8 @@ use rdkafka::message::{Headers, Message};
 use rdkafka::topic_partition_list::TopicPartitionList;
 use rdkafka::util::get_rdkafka_version;
 use std::env;
-use log::{debug, info, warn, error};
+use std::convert::TryInto;
+
 
 // The event handler:
 // - subscribes to the event log (a Kafka topic)
@@ -18,6 +20,41 @@ use log::{debug, info, warn, error};
 struct Config {
     log_level: String,
     topics: Vec<String>,
+    broker: String,
+    group_id: String
+}
+
+struct CustomContext;
+
+impl ClientContext for CustomContext {}
+
+impl ConsumerContext for CustomContext {
+    fn commit_callback(&self, result: KafkaResult<()>, _offsets: &TopicPartitionList) {
+        info!("Committing offsets: {:?}", result);
+    }
+}
+
+async fn handle_event (config : &Config) {
+
+    let Config { group_id, broker, topics, ..} = config;
+    let context = CustomContext;
+
+    let consumer: StreamConsumer<CustomContext> = ClientConfig::new()
+        .set("group.id", group_id)
+        .set("bootstrap.servers", broker)
+        .set("enable.partition.eof", "false")
+        .set("session.timeout.ms", "6000")
+        .set("enable.auto.commit", "true")
+        .set_log_level(RDKafkaLogLevel::Debug)
+        .create_with_context(context)
+        .expect("Consumer creation failed");
+
+    let t: Vec<&str> = topics.iter().map(|x| x.as_str ()).collect();
+
+    consumer
+        .subscribe(&t)
+        .expect("Can't subscribe to specified topics");
+
 }
 
 #[tokio::main]
@@ -25,21 +62,19 @@ async fn main() {
 
     let config = Config {
         log_level: get_env_var ("LOG_LEVEL", Some (String::from ("info"))),
-        topics: string_to_vector (&get_env_var ("TOPICS", Some (String::from ("events"))))
+        topics: string_to_vector (&get_env_var ("KAFKA_TOPICS", Some (String::from ("events")))),
+        broker: get_env_var ("KAFKA_BROKER", Some (String::from ("localhost:9092"))),
+        group_id: get_env_var ("KAFKA_CONSUMER_GROUP_ID", Some (String::from ("event_consumers"))),
     };
 
     env::set_var("RUST_LOG", &config.log_level);
     env_logger::init();
 
-
     let (_, version_s) = get_rdkafka_version();
-    info!("rd_kafka_version: {}", version_s);
+    info!("librdkafka version: {}", version_s);
     info!("{:#?}", &config);
 
-
-
-
-    // info!("Done!");
+    handle_event(&config).await
 }
 
 fn get_env_var (var : &str, default: Option<String> ) -> String {
@@ -55,7 +90,7 @@ fn get_env_var (var : &str, default: Option<String> ) -> String {
 }
 
 fn string_to_vector (s : &str) -> Vec<String> {
-    s.split(",").map(|x| x.parse::<String>().unwrap()).collect()
+    s.split(",").map(|x| String::from (x)).collect()
 }
 
 pub fn print_type_of<T>(_: &T) {
