9e5a0b1f508c7f1f58a93e8c70109319b3729726
filip <fbielejec@gmail.com>
2021-02-02 21:55:58 +0100

    replay events

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,12 @@ use rdkafka::consumer::stream_consumer::StreamConsumer;
 use rdkafka::consumer::{CommitMode, Consumer, ConsumerContext, Rebalance};
 use rdkafka::error::KafkaResult;
 use rdkafka::message::{Headers, Message};
-use rdkafka::topic_partition_list::TopicPartitionList;
-use rdkafka::util::get_rdkafka_version;
-use std::env;
+use rdkafka::topic_partition_list::{TopicPartitionList, Offset};
+use rdkafka::util::{get_rdkafka_version, Timeout} ;
+use std::collections::HashMap;
 use std::convert::TryInto;
-
+use std::env;
+use std::time::{Instant, Duration};
 
 // The event handler:
 // - subscribes to the event log (a Kafka topic)
@@ -19,7 +20,7 @@ use std::convert::TryInto;
 #[derive(Debug, Clone)]
 struct Config {
     log_level: String,
-    topics: Vec<String>,
+    topic: String,
     broker: String,
     group_id: String
 }
@@ -29,31 +30,35 @@ struct CustomContext;
 impl ClientContext for CustomContext {}
 
 impl ConsumerContext for CustomContext {
-    fn commit_callback(&self, result: KafkaResult<()>, _offsets: &TopicPartitionList) {
-        info!("Committing offsets: {:?}", result);
+    fn commit_callback(&self, result: KafkaResult<()>, offsets: &TopicPartitionList) {
+        info!("Committing offsets {:?}", offsets);
     }
 }
 
 async fn handle_event (config : &Config) {
 
-    let Config { group_id, broker, topics, ..} = config;
+    let Config { group_id, broker, topic, ..} = config;
     let context = CustomContext;
 
+    // TODO: set offset
     let consumer: StreamConsumer<CustomContext> = ClientConfig::new()
         .set("group.id", group_id)
         .set("bootstrap.servers", broker)
         .set("enable.partition.eof", "false")
         .set("session.timeout.ms", "6000")
-        .set("enable.auto.commit", "true")
+        .set("enable.auto.commit", "false")
         .set_log_level(RDKafkaLogLevel::Debug)
         .create_with_context(context)
         .expect("Consumer creation failed");
 
-    let t: Vec<&str> = topics.iter().map(|x| x.as_str ()).collect();
-
     consumer
-        .subscribe(&t)
+        .subscribe(&[topic])
         .expect("Can't subscribe to specified topics");
+
+    let mut topic_map: HashMap<(String, i32), Offset> = HashMap::new ();
+    topic_map.insert((String::from (topic), 0), Offset::Beginning);
+    let tpl : TopicPartitionList = TopicPartitionList::from_topic_map (&topic_map).unwrap ();
+    consumer.assign (&tpl);
 
     loop {
         match consumer.recv().await {
@@ -84,7 +89,7 @@ async fn main() {
 
     let config = Config {
         log_level: get_env_var ("LOG_LEVEL", Some (String::from ("info"))),
-        topics: string_to_vector (&get_env_var ("KAFKA_TOPICS", Some (String::from ("events-stream")))),
+        topic: get_env_var ("KAFKA_TOPICS", Some (String::from ("events-stream"))),
         broker: get_env_var ("KAFKA_BROKER", Some (String::from ("localhost:9092"))),
         group_id: get_env_var ("KAFKA_CONSUMER_GROUP_ID", Some (String::from ("event_consumers"))),
     };
